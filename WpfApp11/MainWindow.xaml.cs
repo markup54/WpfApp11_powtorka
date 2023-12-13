d78d269d4ce28fb205e5b09420015fa013942ad9
markup54 <marzenakupka@tlen.pl>
2023-12-13 17:20:07 +0100

    Dodaj pliki projektów.

---
+++ WpfApp11/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp11
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public List<Samochod> Samochody {  get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+            przygotujDane();
+            List<String> list = new List<String>() { "BMW", "Audi", "Opel", "Mercedes" };
+            listaMarek.ItemsSource = list;
+        }
+
+        private void przygotujDane()
+        {
+            Samochody = new List<Samochod>();
+            Samochody.Add(new Samochod("Opel", "Astra", 2015, "czerwony", true));
+            Samochody.Add(new Samochod("BMW", "Astra", 2015, "czerwony", true));
+            Samochody.Add(new Samochod("BMW", "Astra", 2015, "czerwony", true));
+            Samochody.Add(new Samochod("BMW", "Astra", 2015, "czerwony", true));
+            Samochody.Add(new Samochod("BMW", "Astra", 2015, "czerwony", true));
+            Samochody.Add(new Samochod("Audi", "Astra", 2015, "czerwony", true));
+            Samochody.Add(new Samochod("Mercedes", "Astra", 2015, "czerwony", true));
+        }
+    }
+}
---
+++ WpfApp11/Samochod.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp11
+{
+    public class Samochod
+    {
+        public string Marka { get; set; }
+        public string Model { get; set; }
+        public int Rocznik { get; set; }
+        public string Kolor { get; set; }
+        public bool Dostepny { get; set; }
+
+        public Samochod(string marka, string model, int rocznik, string kolor, bool dostepny)
+        {
+            Marka = marka;
+            Model = model;
+            Rocznik = rocznik;
+            Kolor = kolor;
+            Dostepny = dostepny;
+        }
+    }
+}
